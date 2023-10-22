fd645d0d188df9be6442f4a1a1addd76e8be6ccd
Airblow <143172177+Airblow@users.noreply.github.com>
2023-10-22 19:03:56 +0200

    Add project files.

---
+++ inlamning4.5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace inlamning4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string userInput = Console.ReadLine();
+            string finalNumber = "";
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                string character = userInput[i].ToString();
+                int number = int.Parse(character);
+
+                if (number == 9)
+                {
+                    number = 0;
+                }
+                else
+                {
+                    number++;
+                }
+
+                finalNumber += number;
+
+            }
+            Console.WriteLine(finalNumber);
+            Console.ReadKey();
+
+
+        }          
+
+    }
+
+}
